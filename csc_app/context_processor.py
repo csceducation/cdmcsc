7e826e1e5cfdf69a51bdd801b51fc8fe32159984
Senthilsk10 <132420106+Senthilsk10@users.noreply.github.com>
2025-03-03 17:54:07 +0530

    server restart

---
+++ csc_app/context_processor.py
@@ -0,0 +1,8 @@
+def global_context(request):
+    return {
+        "company": "Chidhambaram",
+        "user_ip": request.META.get("REMOTE_ADDR", "Unknown"),
+    }
+    
+company = "Chidhambaram"
+site_pass = "608001"
